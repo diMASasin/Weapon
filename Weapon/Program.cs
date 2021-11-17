4b75fe639fb984b44a655d48f328eac35b8bd4eb
diMASasin <35368058+diMASasin@users.noreply.github.com>
2021-11-17 19:29:22 +0500

    Encapsulation

---
+++ Weapon/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Weapon
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+        private int _bulletsPerShot;
+
+        public Weapon(int damage, int bullets, int bulletsPerShot = 1)
+        {
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage));
+            if (bullets < 0)
+                throw new ArgumentOutOfRangeException(nameof(bullets));
+            if (bullets < 1)
+                throw new ArgumentOutOfRangeException(nameof(bullets));
+
+            _damage = damage;
+            _bullets = bullets;
+            _bulletsPerShot = bulletsPerShot; 
+        }
+
+        public void Fire(Player player)
+        {
+            if(_bullets < _bulletsPerShot)
+                throw new ArgumentOutOfRangeException(nameof(_bullets));
+
+            player.TakeDamage(_damage);
+            _bullets -= _bulletsPerShot;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public Player(int health)
+        {
+            if (health <= 0)
+                throw new ArgumentOutOfRangeException(nameof(health));
+
+            _health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (damage < 0)
+                throw new ArgumentOutOfRangeException(nameof(damage));
+
+            _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            _weapon.Fire(player);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(1);
+            Weapon weapon = new Weapon(2000, 100);
+            Bot bot = new Bot(weapon);
+
+            bot.OnSeePlayer(player);
+        }
+    }
+}
